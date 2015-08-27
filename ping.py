9ded1a2896e82bc30859b372a21e9add4867057a
Danilo Gomes <danilomlgomes@gmail.com>
2015-08-26 22:17:22 -0300

    initial commit

---
+++ ping.py
@@ -0,0 +1,9 @@
+import os
+hostname = "54.84.3.64" #example
+response = os.system("ping -c 1 " + hostname)
+
+#and then check the response...
+if response == 0:
+  print hostname, 'is up!'
+else:
+  print hostname, 'is down!'
